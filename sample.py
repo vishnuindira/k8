eb4098253425be51a884de814d1ae3e52770aa54
Vishnu prasad <vishnuindira123@gmail.com>
2022-01-17 20:43:18 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,9 @@
 
+
+ns=$1
+list=($( kubectl get pods -n $ns  | awk '{print $1}'))
+for object in "${list[@]}" ; do
+
+kubectl get pod $object  -n $ns -o jsonpath='{.spec.containers[].image}{"\n"}'
+done
+
