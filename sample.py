ec2316a8940f4e872bcf2331a0580880f2168e30
Vishnu prasad <vishnuindira123@gmail.com>
2022-06-30 19:11:04 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import json
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route('/name')
+def index():
+    return jsonify({'name': 'vishnu',
+                    'email': 'vishnu@outlook.com'})
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+app.run()
 
-
-ns=$1
-list=($( kubectl get pods -n $ns  | awk '{print $1}'))
-for object in "${list[@]}" ; do
-
-kubectl get pod $object  -n $ns -o jsonpath='{.spec.containers[].image}{"\n"}'
-done
 
